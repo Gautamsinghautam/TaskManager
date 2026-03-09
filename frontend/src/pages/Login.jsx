c29af6fceee04dff9b3472af25879671729fdc91
Gautam Singh <gautamsingh@Gautams-Air.bbrouter>
2026-03-09 11:27:26 +0530

    initial commit

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const Login = () => {
+
+    return <div>Login</div>
+};
+export default Login;
