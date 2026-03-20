eda71de6a44bb9ba0bd5978c198f0b4eb6f53c00
Gautam Singh <gautamsingh@Gautams-MacBook-Air.local>
2026-03-20 18:41:45 +0530

    added middlewares models like notification task user added routes for task and user along with login logouut route

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,9 @@ import express from "express";
 import morgan from "morgan";
 import dbConnection from "./utils/index.js";
 import { errorHandler, routeNotFound } from "./middlewares/errorMiddlewares.js";
+import routes from "./routes/index.js";
 
-const routes= " "
+// const routes= " "
 
 dotenv.config()
 
@@ -26,7 +27,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(morgan("dev"));
-// app.use("/api", routes);
+app.use("/api", routes);
 app.use(routeNotFound);
 app.use(errorHandler);
 
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { isAdminRoute, protectRoute } from "../middlewares/authMiddleware.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser)
+
+// router.get("/get-team", protectRoute, isAdminRoute, getTeamList);
+// router.get("/notification", protectRoute, getNotificationList);
+
+// router.put("/profile", protectRoute, updateUserProfile);
+// router.put("/read-noti", protectRoute, markNotificationRead);
+// router.put("/change-password", protectRoute, changeUserPassword);
+
+// // FOr Admin Only - Admin Routes
+// router
+//     .route("/:id")
+//     .put(protectRoute, isAdminRoute, activeUserProfile)
+//     .delete(protectRoute, isAdminRoute, deleteUserProfile);
+
+export default router;
